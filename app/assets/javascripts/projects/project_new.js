3cd0af1e0770223737c6a83eb74b00a48dbbe666
Ezekiel Kigbo <ekigbo@gitlab.com>
2019-02-26 17:47:41 +1100

    Added tracking data for form inputs
    We want to track usage of features on these
    new project fields, this commit adds the data
    attributes to the relevant controls on the form.

--- app/assets/javascripts/projects/project_new.js
+++ app/assets/javascripts/projects/project_new.js
@@ -5,9 +5,7 @@ import { s__ } from '~/locale';
 import initSettingsPanels from '~/settings_panels';
 import LicenseSelector from '~/blob/template_selectors/license_selector';
 import fileUpload from '~/lib/utils/file_upload';
-import { visibilityOptions } from '~/pages/projects/shared/permissions/constants';
-import setupToggleButtons from '~/toggle_buttons'
-import { parseBoolean } from '../lib/utils/common_utils';
+import setupToggleButtons from '~/toggle_buttons';
 
 let hasUserDefinedProjectPath = false;
 
@@ -48,19 +46,21 @@ const initLicenseField = field => {
   const newProjectFileMediator = {
     selectTemplateFile: (_, query) => {
       field.val(query);
-    }
+    },
   };
 
   return new LicenseSelector({ mediator: newProjectFileMediator }).show();
 };
 
-const ACCESS_LEVEL_OPTIONS_PRIVATE = [[0, 'Enable feature to choose access level']]
-const ACCESS_LEVEL_OPTIONS_PUBLIC = [[10, 'Only Project Members'], [20, 'Everyone With Access']]
+const ACCESS_LEVEL_OPTIONS_PRIVATE = [[0, 'Enable feature to choose access level']];
+const ACCESS_LEVEL_OPTIONS_PUBLIC = [[10, 'Only Project Members'], [20, 'Everyone With Access']];
 
-const getAccessLevelOptionsForFeature = (featureEnabled = false) => 
-  featureEnabled ? ACCESS_LEVEL_OPTIONS_PUBLIC : ACCESS_LEVEL_OPTIONS_PRIVATE;  
+const getAccessLevelOptionsForFeature = (featureEnabled = false) =>
+  featureEnabled ? ACCESS_LEVEL_OPTIONS_PUBLIC : ACCESS_LEVEL_OPTIONS_PRIVATE;
 
-const getProjectVisibilityLevel = (projectVisibilitySelector = `[name='project[visibility_level]']:checked` ) => $(projectVisibilitySelector).val()
+const getProjectVisibilityLevel = (
+  projectVisibilitySelector = `[name='project[visibility_level]']:checked`,
+) => $(projectVisibilitySelector).val();
 
 function updateFeatureVisibilityOptions(
   visibilitySelector, // jquery element
@@ -74,29 +74,38 @@ function updateFeatureVisibilityOptions(
   }
 
   const accessLevelOptions = getAccessLevelOptionsForFeature(featureEnabled);
-  
-  // if the feature is enabled, we should always default to the internal access level even if visibility is private
-  const visibility = (!projectVisibilityLevel && featureEnabled) ? visibilityOptions.INTERNAL : projectVisibilityLevel 
-
-  // set the selected index for the feature selector
-  const computedSelectedIndex = 
-    accessLevelOptions.findIndex(opt => opt[0] == projectVisibilityLevel)
-  const selectedIndex = computedSelectedIndex > -1 ? computedSelectedIndex : 0
+  const computedSelectedIndex = accessLevelOptions.findIndex(
+    opt => opt[0] === projectVisibilityLevel,
+  );
+  const selectedIndex = computedSelectedIndex > -1 ? computedSelectedIndex : 0;
 
   const options = accessLevelOptions.map((alo, index) => {
-    const selected = selectedIndex === index ? 'selected="selected"' : ''
-    return `<option ${selected} value="${alo[0]}">${alo[1]}</option>`
+    const selected = selectedIndex === index ? 'selected="selected"' : '';
+    return `<option ${selected} value="${alo[0]}">${alo[1]}</option>`;
   });
-  $(visibilitySelector).html(options);
 
+  $(visibilitySelector).html(options);
 }
 
 function onToggleFeatureSetting(featureEnabled, toggle) {
   const projectVisibilityLevel = getProjectVisibilityLevel();
-  const toggleContext = $(toggle).closest('.project-feature-controls')
+  const toggleContext = $(toggle).closest('.project-feature-controls');
   const visibilitySelector = $('.select-control', toggleContext);
   updateFeatureVisibilityOptions(visibilitySelector, projectVisibilityLevel, featureEnabled);
-  // TOOD: update toggle aria-label
+  $(toggle).attr('aria-label', featureEnabled ? 'Toggle Status: ON' : 'Toggle Status: OFF');
+}
+
+function onProjectVisitbilityChange(featuresSelectors) {
+  const projectVisibilityLevel = getProjectVisibilityLevel();
+
+  featuresSelectors.forEach(context => {
+    const featureEnabled = parseBoolean(
+      context.querySelector('.js-project-feature-toggle-input').getAttribute('value'),
+    );
+
+    const visibilitySelector = $('.select-control', context);
+    updateFeatureVisibilityOptions(visibilitySelector, projectVisibilityLevel, featureEnabled);
+  });
 }
 
 const bindEvents = () => {
@@ -112,6 +121,7 @@ const bindEvents = () => {
   const $projectTemplateButtons = $('.project-templates-buttons');
   const $projectName = $('.tab-pane.active #project_name');
   const $projectVisibilityLevel = $('.visibility-level-setting [name="project[visibility_level]"]');
+  const $projectFeatures = document.querySelectorAll('.project-feature-row');
 
   if ($newProjectForm.length !== 1) {
     return;
@@ -291,18 +301,7 @@ const bindEvents = () => {
     hasUserDefinedProjectPath = $projectPath.val().trim().length > 0;
   });
 
-  $projectVisibilityLevel.on('change', e => {  
-    const projectVisibilityLevel = getProjectVisibilityLevel();
-    const features = document.querySelectorAll('.project-feature-row');
-    features.forEach(context => {
-      const featureEnabled = parseBoolean(context
-        .querySelector('.js-project-feature-toggle-input')
-        .getAttribute('value'));
-
-      const visibilitySelector = $('.select-control', context);
-      updateFeatureVisibilityOptions(visibilitySelector, projectVisibilityLevel, featureEnabled);
-    });
-  });
+  $projectVisibilityLevel.on('change', onProjectVisitbilityChange($projectFeatures));
 
   // select all the hidden fields
 
@@ -310,26 +309,6 @@ const bindEvents = () => {
     expandedText: 'Hide avatar, license and features settings',
     collapsedText: 'Show avatar, license and features settings',
   });
-
-  // TODO: Fixup behaviour for the toggles
-  // - Dropdown should be available with all options on load
-  // - Hide toggles on load
-  // - If js
-  //   - show toggles
-  //   - dropdown should default to 0 (enable ....)
-  //   - if a toggle is on, then set the options to (10 only members, 20 everyone..)
-  // const $issuesAccessLevelHiddenField = document.querySelector(
-  //   `[name='project[project_feature_attributes][issues_access_level]]`,
-  // );
-  // const $repositoryAccessLevelHiddenField = document.querySelector(
-  //   `[name='project[project_feature_attributes][repository_access_level]]`,
-  // );
-  // const $wikiAccessLevelHiddenField = document.querySelector(
-  //   `[name='project[project_feature_attributes][wiki_access_level]]`,
-  // );
-  // const $snippetsAccessLevelHiddenField = document.querySelector(
-  //   `[name='project[project_feature_attributes][snippets_access_level]]`,
-  // );
 
   const $toggleContainer = document.querySelector('.project-feature-settings');
   setupToggleButtons($toggleContainer, onToggleFeatureSetting);
--- spec/features/projects/new_project_spec.rb
+++ spec/features/projects/new_project_spec.rb
@@ -56,7 +56,7 @@ describe 'New project' do
       find('.js-settings-toggle')
     end
 
-    it 'will hide the extra settings when i click the show / hide toggle' do 
+    it 'will hide the extra settings when i click the show / hide toggle' do
       find_button('Hide avatar, license and features settings').click
       expect(page).not_to have_content('Project description')
       expect(page).not_to have_content('Project avatar')
@@ -65,7 +65,7 @@ describe 'New project' do
       expect(page).not_to have_content('Lightweight issue tracking system for this project')
       expect(page).not_to have_content('View and edit files in this project')
       expect(page).not_to have_content('Pages for project documentation')
-      expect(page).not_to have_content('Share code pastes with others out of Git repository')      
+      expect(page).not_to have_content('Share code pastes with others out of Git repository')
       expect(page).not_to have_css('.settings.expanded')
     end
   end
@@ -126,12 +126,12 @@ describe 'New project' do
   context 'Feature settings', :js do
     context 'with private visibility' do
       before do
-        visit new_project_path            
+        visit new_project_path
         choose("Private")
-        expect(find_field("project[visibility_level]", checked: true).value.to_i).to eq(Gitlab::VisibilityLevel::PRIVATE)          
+        expect(find_field("project[visibility_level]", checked: true).value.to_i).to eq(Gitlab::VisibilityLevel::PRIVATE)
 
         page.all('.project-feature-controls').each do |el|
-          el.find('.project-feature-toggle').click()
+          el.find('.project-feature-toggle').click
         end
       end
 
@@ -146,23 +146,21 @@ describe 'New project' do
           expect(el.find('.project-repo-select', visible: false).value.to_i).to eq(Gitlab::VisibilityLevel::INTERNAL)
         end
       end
-      
-      # TODO: This is failing, fix the js
-      it 'sets each feature select to "Only project members" when the feature is enabled' do      
+
+      it 'sets each feature select to "Only project members" when the feature is enabled' do
         page.all('.project-feature-controls').each do |el|
           expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Only Project Members')
         end
       end
 
-
       context 'with each feature enabled' do
         before do
-          visit new_project_path         
+          visit new_project_path
           choose("Private")
 
-          page.all('.project-feature-controls').each do |el|        
-            el.find('.project-feature-toggle').click()          
-          end          
+          page.all('.project-feature-controls').each do |el|
+            el.find('.project-feature-toggle').click
+          end
         end
 
         it 'switching to internal' do
@@ -181,13 +179,13 @@ describe 'New project' do
             expect(el.find('.project-repo-select', visible: false).value.to_i).to eq(Gitlab::VisibilityLevel::PUBLIC)
             expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Everyone With Access')
           end
-        end      
-      end      
+        end
+      end
     end
 
     context 'with internal visibility' do
       before do
-        visit new_project_path  
+        visit new_project_path
 
         choose("Internal")
         expect(find_field("project[visibility_level]", checked: true).value.to_i).to eq(Gitlab::VisibilityLevel::INTERNAL)
@@ -195,31 +193,31 @@ describe 'New project' do
 
       it 'enables feature select boxes when the feature is also enabled' do
         page.all('.project-feature-controls').each do |el|
-          el.find('.project-feature-toggle').click()
+          el.find('.project-feature-toggle').click
           expect(el.find('.project-repo-select')).not_to be_disabled
         end
       end
 
       it 'defaults each feature select to "Only Project Members" when enabled' do
         page.all('.project-feature-controls').each do |el|
-          el.find('.project-feature-toggle').click()
+          el.find('.project-feature-toggle').click
           expect(el.find('.project-repo-select', visible: false).value.to_i).to eq(Gitlab::VisibilityLevel::INTERNAL)
-          expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Only Project Members') 
+          expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Only Project Members')
         end
       end
 
       context 'with each feature enabled' do
         before do
-          visit new_project_path         
+          visit new_project_path
 
-          choose('Internal')     
-          page.all('.project-feature-controls').each do |el|        
-            el.find('.project-feature-toggle').click()          
+          choose('Internal')
+          page.all('.project-feature-controls').each do |el|
+            el.find('.project-feature-toggle').click
             expect(el.find('.project-repo-select')).not_to be_disabled
             expect(el.find('.project-repo-select', visible: false).value.to_i).to eq(Gitlab::VisibilityLevel::INTERNAL)
             expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Only Project Members')
           end
-        end 
+        end
 
         it 'switching to private' do
           choose("Private")
@@ -238,46 +236,45 @@ describe 'New project' do
             expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Everyone With Access')
           end
         end
-      end      
+      end
     end
 
     context 'with public visibility' do
       before do
-        visit new_project_path         
+        visit new_project_path
 
-        choose('Public')   
+        choose('Public')
         expect(find_field("project[visibility_level]", checked: true).value.to_i).to eq(Gitlab::VisibilityLevel::PUBLIC)
       end
 
       it 'enables feature select boxes when the feature is also enabled' do
         page.all('.project-feature-controls').each do |el|
-          el.find('.project-feature-toggle').click()
+          el.find('.project-feature-toggle').click
           expect(el.find('.project-repo-select')).not_to be_disabled
         end
       end
 
       it 'defaults each feature select to "Everyone with access" when enabled' do
         page.all('.project-feature-controls').each do |el|
-          el.find('.project-feature-toggle').click()
+          el.find('.project-feature-toggle').click
           expect(el.find('.project-repo-select', visible: false).value.to_i).to eq(Gitlab::VisibilityLevel::PUBLIC)
-          expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Everyone With Access') 
+          expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Everyone With Access')
         end
       end
 
-
       context 'with each feature enabled' do
         before do
-          visit new_project_path         
+          visit new_project_path
 
           choose('Public')
           page.all('.project-feature-controls').each do |el|
-            el.find('.project-feature-toggle').click()          
+            el.find('.project-feature-toggle').click
             expect(el.find('.project-repo-select')).not_to be_disabled
             expect(el.find('.project-repo-select', visible: false).value.to_i).to eq(Gitlab::VisibilityLevel::PUBLIC)
             expect(el.find('.project-repo-select').find('option[selected]').text).to eq('Everyone With Access')
-          end   
-        end          
-        
+          end
+        end
+
         it 'switching to internal' do
           choose("Internal")
           page.all('.project-feature-controls').each do |el|
@@ -287,7 +284,7 @@ describe 'New project' do
           end
         end
 
-        it 'switching to private' do          
+        it 'switching to private' do
           choose("Private")
           page.all('.project-feature-controls').each do |el|
             expect(el.find('.project-repo-select')).to be_disabled
@@ -295,7 +292,7 @@ describe 'New project' do
             expect(el.find('.project-repo-select').find('option[selected]', visible: false).text).to eq('Only Project Members')
           end
         end
-      end 
+      end
     end
   end
 
@@ -312,7 +309,7 @@ describe 'New project' do
       find('#create-from-template-pane').click
       first('.choose-template').click
 
-      page.within '.project-fields-form' do 
+      page.within '.project-fields-form' do
         expect(page).not_to have_css('input#project_initialize_with_readme')
         expect(page).not_to have_content('Initialize repository with a README')
       end
