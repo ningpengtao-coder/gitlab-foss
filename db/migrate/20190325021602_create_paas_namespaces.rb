e5ef9b70345c04640ffd165d1d61d644f6d38a52
Thong Kuah <tkuah@gitlab.com>
2019-03-27 22:32:01 +1300

    Revert "Fix FK type mismatchgin mySQL"
    This reverts commit 27eaebd54279e1c13e60f50e243da885452e28b9.

--- db/migrate/20190325021602_create_paas_namespaces.rb
+++ db/migrate/20190325021602_create_paas_namespaces.rb
@@ -7,7 +7,7 @@ class CreatePaasNamespaces < ActiveRecord::Migration[5.0]
     create_table :paas_namespaces, id: :bigserial do |t|
       t.string :name, null: false
       t.references :project, index: true, foreign_key: { on_delete: :nullify }
-      t.references :cluster, type: :bigint, null: false, index: true, foreign_key: { on_delete: :cascade }
+      t.references :cluster, null: false, index: true, foreign_key: { on_delete: :cascade }
 
       t.timestamps_with_timezone null: false
     end
