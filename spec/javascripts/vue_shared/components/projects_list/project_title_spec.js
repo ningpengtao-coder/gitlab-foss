008860a636354c93d267fec8c275df29f5434207
Ezekiel Kigbo <ekigbo@gitlab.com>
2019-04-26 18:46:33 +0200

    Updated specs for project title component
    Signed-off-by: Ezekiel Kigbo <ekigbo@gitlab.com>

--- spec/javascripts/vue_shared/components/projects_list/project_title_spec.js
+++ spec/javascripts/vue_shared/components/projects_list/project_title_spec.js
@@ -1,17 +1,21 @@
 import Vue from 'vue';
-import ProjectListItem from '~/vue_shared/components/projects_list/project_list_item.vue';
+import ProjectTitle from '~/vue_shared/components/projects_list/project_title.vue';
 
 import mountComponent from 'spec/helpers/vue_mount_component_helper';
 
 // TODO: move to shallow mount / vue test utils ??
 
 loadJSONFixtures('projects.json');
-const projects = getJSONFixture('projects.json');
-const ownedProject = projects[0];
-const selectedProject = projects[1];
+const selectedProject = getJSONFixture('projects.json')[1];
+console.log('selectedProject', selectedProject);
+const {
+  namespace: { name: namespaceName },
+  name,
+  path_with_namespace: pathWithNamespace,
+} = selectedProject;
 
-const createComponent = (props, defaultComponent = ProjectListItem) => {
-  const Component = Vue.extend(defaultComponent);
+const createComponent = props => {
+  const Component = Vue.extend(ProjectTitle);
 
   return mountComponent(Component, props);
 };
@@ -20,16 +24,48 @@ describe('ProjectTitle', () => {
   let vm;
 
   beforeEach(() => {
-    vm = createComponent({ project: selectedProject });
+    vm = createComponent({
+      namespace: namespaceName,
+      name,
+      pathWithNamespace,
+    });
   });
 
   afterEach(() => {
     vm.$destroy();
   });
 
-  it(`renders the project namespace name if the 'owner' property is not available`, () => {
-    expect(vm.$el.querySelector('.namespace-name').innerText).toEqual(
-      `${selectedProject.namespace.name} /`,
-    );
+  describe('computed', () => {
+    describe('namespaceName', () => {
+      it('should add a backslash after the namespace', () => {
+        expect(vm.namespaceName).toBe(`${namespaceName} /`);
+      });
+    });
+
+    describe('path', () => {
+      it('should be a relative url', () => {
+        expect(vm.path).toBe(`/${pathWithNamespace}`);
+      });
+    });
+  });
+
+  describe('template', () => {
+    it(`renders the project name`, () => {
+      expect(vm.$el.querySelector('.project-name').innerText).toEqual(name);
+    });
+
+    it(`renders the project namespace`, () => {
+      expect(vm.$el.querySelector('.namespace-name').innerText).toEqual(`${namespaceName} /`);
+    });
+
+    it(`renders the full project title correctly`, () => {
+      expect(vm.$el.querySelector('.project-full-name').innerText).toEqual(
+        `${namespaceName} / ${name}`,
+      );
+    });
+
+    it(`renders a link to the project path`, () => {
+      expect(vm.$el.querySelector('h2 a').getAttribute('href')).toBe(`/${pathWithNamespace}`);
+    });
   });
 });
