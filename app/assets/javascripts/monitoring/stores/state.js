43ec71b7db5ed54c594924a24a35c6bef01a55ec
Jose Vargas <jvargas@gitlab.com>
2019-04-30 17:32:13 -0500

    Refactor the store/state logic to vuex
    Currently this is not implemented in the main component
    basic specs for the actions and getters are in this commit

---
+++ app/assets/javascripts/monitoring/stores/state.js
@@ -0,0 +1,21 @@
+export default () => ({
+  hasMetrics: false,
+  showPanels: true,
+  documentationPath: null, // From the vuex docs, strings should be null by default
+  settingsPath: null, // From dashboard.vue, props
+  clustersPath: null,
+  tagsPath: null,
+  projectPath: null,
+  metricsEndpoint: null,
+  deploymentEndpoint: null,
+  emptyGettingStartedSvgPath: null,
+  emptyLoadingSvgPath: null,
+  emptyNoDataSvgPath: null,
+  emptyUnableToConnectSvgPath: null,
+  environmentsEndpoint: null,
+  currentEnvironmentName: null, // Finish props
+  showEmptyState: true, // From the data
+  emptyState: 'loading',
+  selectedTimeWindow: null, // finish data section
+  groups: [], // NEW
+});
