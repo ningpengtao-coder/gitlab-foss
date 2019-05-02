fe5da23b1eebc0668dba27d22d7570cb319e4944
sarahghp <sarah.groff.palermo@gmail.com>
2019-05-02 11:36:35 -0400

    Add initial test

--- spec/features/review_app/user_loads_toolbar_spec.rb
+++ spec/features/review_app/user_loads_toolbar_spec.rb
@@ -3,11 +3,28 @@ require 'spec_helper'
 describe 'Toolbar', :js do
 
   describe 'injected toolbar' do
+    add_script = %q(
+      const scriptEl = document.createElement('script');
+      scriptEl.setAttribute('src', 'https://host-island.sarahghp.now.sh/review-toolbar.js');
+      scriptEl.setAttribute('data-project' , '11790219');
+      scriptEl.setAttribute('data-discussion' , '1');
+      scriptEl.setAttribute('data-name' , 'ReviewAppToolbar');
+      scriptEl.setAttribute('data-mr-url' , 'https://gitlab.com');
+      document.head.appendChild(scriptEl);
+    )
+
+    load_script = %q(
+      const newLoad = new Event('load');
+      window.dispatchEvent(newLoad);
+    )
+
     it 'loads the login screen' do
       visit root_path
       find('body')
       evaluate_script("document.body.innerHTML = 'hi winnie and sarah, but for real'")
-      live_debug
+      evaluate_script(add_script)
+      evaluate_script(load_script)
+      expect(find('#gitlab-review-container')).to exist
     end
   end
 end
