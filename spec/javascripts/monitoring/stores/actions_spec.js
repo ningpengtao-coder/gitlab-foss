56abb99802089bf7fcb223f16054c941d9032609
Simon Knox <psimyn@gmail.com>
2019-05-03 13:45:44 +1000

    Add some action and mutation specs

--- spec/javascripts/monitoring/stores/actions_spec.js
+++ spec/javascripts/monitoring/stores/actions_spec.js
@@ -1,20 +1,100 @@
+import axios from '~/lib/utils/axios_utils';
+import MockAdapter from 'axios-mock-adapter';
 import store from '~/monitoring/stores'
 import * as types from '~/monitoring/stores/mutation_types';
-import testAction from '../../helpers/vuex_action_helper';
 import { resetStore } from '../helpers';
+import MonitoringMock, { deploymentData, environmentData } from '../mock_data';
+import {
+  fetchDeploymentsData,
+  fetchEnvironmentsData,
+  requestMetricsData,
+} from '~/monitoring/stores/actions';
 
 describe('Monitoring store actions', () => {
+  let mock;
+
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+  });
+
   afterEach(() => {
     resetStore(store);
+    mock.restore();
   });
 
   describe('requestMetricsData', () => {
-    it('sets emptyState to loading', done => {
-      store
-        .dispatch('requestMetricsData')
+    it('sets emptyState to loading', () => {
+      const commit = jasmine.createSpy();
+      const state = store.state;
+
+      requestMetricsData({ state, commit });
+
+      expect(commit).toHaveBeenCalledWith(types.REQUEST_METRICS_DATA);
+    });
+  });
+
+  describe('fetchDeploymentsData', () => {
+    it('commits RECEIVE_DEPLOYMENTS_DATA_SUCCESS on error', (done) => {
+      const commit = jasmine.createSpy();
+      const state = store.state;
+      state.deploymentEndpoint = '/success'
+
+      mock.onGet(state.deploymentEndpoint).reply(200, {
+        deployments: deploymentData,
+      });
+
+      fetchDeploymentsData({ state, commit })
         .then(() => {
-          expect(store.state.emptyState).toEqual('loading');
-          expect(store.state.showEmptyState).toBeTruthy();
+          expect(commit).toHaveBeenCalledWith(types.RECEIVE_DEPLOYMENTS_DATA_SUCCESS, deploymentData);
+          done();
+        })
+        .catch(done.fail);
+    });
+
+    it('commits RECEIVE_DEPLOYMENTS_DATA_FAILURE on error', (done) => {
+      const commit = jasmine.createSpy();
+      const state = store.state;
+      state.deploymentEndpoint = '/error'
+
+      mock.onGet(state.deploymentEndpoint).reply(500);
+
+      fetchDeploymentsData({ state, commit })
+        .then(() => {
+          expect(commit).toHaveBeenCalledWith(types.RECEIVE_DEPLOYMENTS_DATA_FAILURE);
+          done();
+        })
+        .catch(done.fail);
+    });
+  });
+
+  describe('fetchEnvironmentsData', () => {
+    it('commits RECEIVE_ENVIRONMENTS_DATA_SUCCESS on error', (done) => {
+      const commit = jasmine.createSpy();
+      const state = store.state;
+      state.environmentsEndpoint = '/success'
+
+      mock.onGet(state.environmentsEndpoint).reply(200, {
+        environments: environmentData,
+      });
+
+      fetchEnvironmentsData({ state, commit })
+        .then(() => {
+          expect(commit).toHaveBeenCalledWith(types.RECEIVE_ENVIRONMENTS_DATA_SUCCESS, environmentData);
+          done();
+        })
+        .catch(done.fail);
+    });
+
+    it('commits RECEIVE_ENVIRONMENTS_DATA_FAILURE on error', (done) => {
+      const commit = jasmine.createSpy();
+      const state = store.state;
+      state.environmentsEndpoint = '/error'
+
+      mock.onGet(state.environmentsEndpoint).reply(500);
+
+      fetchEnvironmentsData({ state, commit })
+        .then(() => {
+          expect(commit).toHaveBeenCalledWith(types.RECEIVE_ENVIRONMENTS_DATA_FAILURE);
           done();
         })
         .catch(done.fail);
---
+++ spec/javascripts/monitoring/stores/mutations_spec.js
@@ -0,0 +1,23 @@
+import { createStore } from '~/monitoring/stores';
+import mutations from '~/monitoring/stores/mutations';
+import * as types from '~/monitoring/stores/mutation_types';
+import { metricsGroupsAPIResponse } from '../mock_data';
+
+describe('Monitoring mutations', () => {
+  describe(types.RECEIVE_METRICS_DATA_SUCCESS, () => {
+    it('normalizes values', () => {
+      const state = {
+        groups: []
+      };
+      const groups = metricsGroupsAPIResponse.data;
+
+      mutations[types.RECEIVE_METRICS_DATA_SUCCESS](state, groups);
+
+      const expectedTimestamp = '2017-05-25T08:22:34.925Z';
+      const expectedValue = 0.0010794445585559514;
+      const [timestamp, value] = state.groups[0].metrics[0].queries[0].result[0].values[0];
+      expect(timestamp).toEqual(expectedTimestamp);
+      expect(value).toEqual(expectedValue);
+    });
+  });
+});
