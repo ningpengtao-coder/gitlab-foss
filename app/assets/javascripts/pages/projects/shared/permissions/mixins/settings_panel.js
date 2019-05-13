47551c5e7ddbb5915bbdcd92048c36efeeef199a
Constance Okoghenun <constanceokoghenun@gmail.com>
2019-05-13 16:09:11 -0500

    Added isEE property to settings panel mixin

--- app/assets/javascripts/pages/projects/shared/permissions/mixins/settings_panel.js
+++ app/assets/javascripts/pages/projects/shared/permissions/mixins/settings_panel.js
@@ -1,4 +1,11 @@
+import { isEE } from '~/lib/utils/common_utils';
+
 export default {
+  computed: {
+    isEE() {
+      return isEE();
+    },
+  },
   data() {
     return {
       packagesAvailable: false,
