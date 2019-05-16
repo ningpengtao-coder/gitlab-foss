dda70eece2954084fca6ac75c2ba538f9e2bf8f8
Ezekiel Kigbo <ekigbo@gitlab.com>
2019-05-16 16:16:33 -0500

    Scaffold ci pipeline status vue component

---
+++ app/assets/javascripts/vue_shared/components/projects_list/project_ci_pipeline_status.vue
@@ -0,0 +1,13 @@
+<script>
+// TODO: should this poll for latest status?
+
+export default {
+  components: {},
+  props: {},
+  data() {},
+  computed: {},
+};
+</script>
+<template>
+  <div class="icon-wrapper pipeline-status"></div>
+</template>
---
+++ spec/javascripts/vue_shared/components/projects_list/project_ci_pipeline_status.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import ProjectCiPipelineStatus from '~/vue_shared/components/projects_list/project_ci_pipeline_status.vue';
+import mountComponent from 'spec/helpers/vue_mount_component_helper';
+
+loadJSONFixtures('projects.json');
+let vm;
+
+const createComponent = (props, defaultComponent = ProjectCiPipelineStatus) => {
+  const Component = Vue.extend(defaultComponent);
+
+  return mountComponent(Component, props);
+};
+
+describe('ProjectCiPipelineStatus', () => {
+  beforeEach(() => {
+    vm = createComponent({}, ProjectCiPipelineStatus);
+  });
+
+  afterEach(() => {
+    vm.$destroy();
+  });
+});
