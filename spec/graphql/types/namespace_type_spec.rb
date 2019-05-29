e34edc5c9126be6cd30335538af08719e40b3b0f
Alessio Caiazza <acaiazza@gitlab.com>
2019-05-29 14:42:10 +0200

    Fix spec failure

--- spec/graphql/types/namespace_type_spec.rb
+++ spec/graphql/types/namespace_type_spec.rb
@@ -5,5 +5,5 @@ require 'spec_helper'
 describe GitlabSchema.types['Namespace'] do
   it { expect(described_class.graphql_name).to eq('Namespace') }
 
-  it { expect(described_class).to have_graphql_field(:all_projects) }
+  it { expect(described_class).to have_graphql_field(:projects) }
 end
