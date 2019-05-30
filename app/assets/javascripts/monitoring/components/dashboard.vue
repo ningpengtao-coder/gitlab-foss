9a557a3ac18801b72c15ceb87a4e0eadc6a44975
Simon Knox <psimyn@gmail.com>
2019-05-30 21:00:22 +1000

    Make EE-only props not required in CE
    The metricsPath values only exist in EE metrics_data

--- app/assets/javascripts/monitoring/components/dashboard.vue
+++ app/assets/javascripts/monitoring/components/dashboard.vue
@@ -117,11 +117,13 @@ export default {
     },
     customMetricsPath: {
       type: String,
-      required: true,
+      required: false,
+      default: '',
     },
     validateQueryPath: {
       type: String,
-      required: true,
+      required: false,
+      default: '',
     },
   },
   data() {
