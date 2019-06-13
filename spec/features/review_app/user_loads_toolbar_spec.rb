276555f7fcc57a9b749f30bcc18eda9c5f110042
sarahghp <sarah.groff.palermo@gmail.com>
2019-06-13 12:31:01 -0400

    Nonworking code, for example

--- spec/features/review_app/user_loads_toolbar_spec.rb
+++ spec/features/review_app/user_loads_toolbar_spec.rb
@@ -1,14 +1,14 @@
+# frozen_string_literal: true
 require 'spec_helper'
 
 describe 'Toolbar', :js do
-
   describe 'injected toolbar' do
     add_script = %q(
       const scriptEl = document.createElement('script');
       scriptEl.setAttribute('src', 'https://host-island.sarahghp.now.sh/review-toolbar.js');
-      scriptEl.setAttribute('data-project' , '11790219');
-      scriptEl.setAttribute('data-discussion' , '1');
-      scriptEl.setAttribute('data-name' , 'ReviewAppToolbar');
+      scriptEl.setAttribute('data-project-id' , '11790219');
+      scriptEl.setAttribute('data-merge-request-id' , '1');
+      scriptEl.setAttribute('id' , 'review-app-toolbar-script');
       scriptEl.setAttribute('data-mr-url' , 'https://gitlab.com');
       document.head.appendChild(scriptEl);
     )
