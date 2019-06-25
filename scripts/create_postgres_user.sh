4d4d769bfb3a9a7c978c0335ee0fbbf4134ea9d8
Aleksei Lipniagov <alipniagov@gitlab.com>
2019-06-25 13:55:29 +0300

    Create user git

--- scripts/create_postgres_user.sh
+++ scripts/create_postgres_user.sh
@@ -3,4 +3,6 @@
 psql -h postgres -U postgres postgres <<EOF
 CREATE USER gitlab;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO gitlab;
+CREATE USER git;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO gitlab;
 EOF
