b3a3ef17905074f0ffa37ec19dcbe338fc11fe15
Donald Cook <dcook@gitlab.com>
2019-06-28 13:24:35 -0500

    Added MAX_ASSIGNEES_RENDER constant

--- app/assets/javascripts/issues/constants.js
+++ app/assets/javascripts/issues/constants.js
@@ -6,3 +6,4 @@ export const ISSUE_STATES = {
 export const ACTIVE_TAB_CLASS = 'active';
 export const ISSUES_PER_PAGE = 20;
 export const DASHBOARD_PAGE_NAME = 'dashboard';
+export const MAX_ASSIGNEES_RENDER = 3;
