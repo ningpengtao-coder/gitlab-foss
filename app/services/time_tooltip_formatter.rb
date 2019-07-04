c42e27840997108b5a8788cfa47ab989ec0db6c0
Etienne Baqué <ebaque@gitlab.com>
2019-07-04 11:44:30 -0400

    Refactored time_ago_with_tooltip method into its own class

---
+++ app/services/time_tooltip_formatter.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class TimeTooltipFormatter
+  attr_accessor :time, :placement, :html_class, :short_format, :timezoned
+
+  TIME_FORMAT = '%b %d, %Y'
+
+  def initialize(time:, placement:, html_class:, short_format:, timezoned:)
+    @time = time
+    @placement = placement
+    @html_class = html_class
+    @short_format = short_format
+    @timezoned = timezoned
+  end
+
+  def time_format
+    TIME_FORMAT
+  end
+
+  def tooltip_format
+    "timeago_tooltip#{@timezoned ? '_tz' : ''}".to_sym
+  end
+
+  def css_classes
+    classes = [@short_format ? 'js-short-timeago' : 'js-timeago']
+    classes << @html_class unless @html_class.blank?
+    classes.join(' ')
+  end
+
+  def time_for_tooltip
+    t = @time.to_time
+    @timezoned ? t.localtime : t.in_time_zone
+  end
+
+  def time_to_datetime
+    @time.to_time.getutc.iso8601
+  end
+
+  def element_data
+    {
+      toggle: 'tooltip',
+      placement: @placement,
+      container: 'body'
+    }
+  end
+end
