1aa1719e5c6d43881cb86e42f16e2edd8867d1cb
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-07-10 09:02:16 -0400

    reduce duplicated code in js assets

--- app/assets/javascripts/pages/projects/wiki_directories/index.js
+++ app/assets/javascripts/pages/projects/wiki_directories/index.js
@@ -1,10 +1,2 @@
-import $ from 'jquery';
-import ShortcutsWiki from '~/behaviors/shortcuts/shortcuts_wiki';
-import Wikis from '../wikis/wikis';
-import GLForm from '../../../gl_form';
-
-document.addEventListener('DOMContentLoaded', () => {
-  new Wikis(); // eslint-disable-line no-new
-  new ShortcutsWiki(); // eslint-disable-line no-new
-  new GLForm($('.wiki-form')); // eslint-disable-line no-new
-});
+// currently, this controller inherits all behaviors from wikis
+import '../wikis/index';
