29a78a8f1a6a3e01e594d207cc2d1c7d12552810
sarahghp <sarah.groff.palermo@gmail.com>
2019-07-19 18:34:00 -0600

    Address linter complaints

--- app/assets/javascripts/visual_review_toolbar/store/events.js
+++ app/assets/javascripts/visual_review_toolbar/store/events.js
@@ -59,8 +59,9 @@ const initializeGlobalListeners = () => {
       saveComment();
     } catch (err) {
       // in this case, if saving the comment fails,
-      // the user will be warned
+      // the user will be warned, as recommended by MDN: https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload#Example
       event.preventDefault();
+      /* eslint-disable-next-line no-param-reassign */
       event.returnValue = '';
     }
   });
