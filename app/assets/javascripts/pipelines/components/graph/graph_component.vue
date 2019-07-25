54edcd3daf660c907cbedb30e97ea519085f5df4
mfluharty <mfluharty@gitlab.com>
2019-07-25 17:21:03 -0600

    Extend pipeline graph scroll area to full width
    Use debounced resize function to set left padding inside graph
    Set padding such that the left edge of the pipeline lines up with
    the fixed-width container above it in the default scroll position
    Also trigger padding recalculation on layout change

--- app/assets/javascripts/pipelines/components/graph/graph_component.vue
+++ app/assets/javascripts/pipelines/components/graph/graph_component.vue
@@ -2,6 +2,10 @@
 import { GlLoadingIcon } from '@gitlab/ui';
 import StageColumnComponent from './stage_column_component.vue';
 import GraphMixin from '../../mixins/graph_component_mixin';
+import { debounceByAnimationFrame } from '~/lib/utils/common_utils';
+
+let debouncedResize;
+let sidebarMutationObserver;
 
 export default {
   components: {
@@ -9,28 +13,67 @@ export default {
     GlLoadingIcon,
   },
   mixins: [GraphMixin],
+  data() {
+    return {
+      leftPadding: 0,
+    };
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', debouncedResize);
+
+    if (sidebarMutationObserver) {
+      sidebarMutationObserver.disconnect();
+    }
+  },
+  created() {
+    debouncedResize = debounceByAnimationFrame(this.setLeftPadding);
+    window.addEventListener('resize', debouncedResize);
+  },
+  mounted() {
+    this.setLeftPadding();
+
+    sidebarMutationObserver = new MutationObserver(this.handleLayoutChange);
+    sidebarMutationObserver.observe(document.querySelector('.layout-page'), {
+      attributes: true,
+      childList: false,
+      subtree: false,
+    });
+  },
+  methods: {
+    setLeftPadding() {
+      const container = document.querySelector('.content .container-limited');
+      if (!container) return;
+      this.leftPadding = container.offsetLeft;
+    },
+    handleLayoutChange() {
+      // wait until animations finish, then recalculate padding
+      window.setTimeout(this.setLeftPadding, 300);
+    },
+  },
 };
 </script>
 <template>
   <div class="build-content middle-block js-pipeline-graph">
     <div class="pipeline-visualization pipeline-graph pipeline-tab-content">
-      <div v-if="isLoading" class="m-auto"><gl-loading-icon :size="3" /></div>
+      <div :style="{ paddingLeft: `${leftPadding}px` }">
+        <div v-if="isLoading" class="m-auto"><gl-loading-icon :size="3" /></div>
 
-      <ul v-if="!isLoading" class="stage-column-list">
-        <stage-column-component
-          v-for="(stage, index) in graph"
-          :key="stage.name"
-          :class="{
-            'append-right-48': shouldAddRightMargin(index),
-          }"
-          :title="capitalizeStageName(stage.name)"
-          :groups="stage.groups"
-          :stage-connector-class="stageConnectorClass(index, stage)"
-          :is-first-column="isFirstColumn(index)"
-          :action="stage.status.action"
-          @refreshPipelineGraph="refreshPipelineGraph"
-        />
-      </ul>
+        <ul v-if="!isLoading" class="stage-column-list">
+          <stage-column-component
+            v-for="(stage, index) in graph"
+            :key="stage.name"
+            :class="{
+              'append-right-48': shouldAddRightMargin(index),
+            }"
+            :title="capitalizeStageName(stage.name)"
+            :groups="stage.groups"
+            :stage-connector-class="stageConnectorClass(index, stage)"
+            :is-first-column="isFirstColumn(index)"
+            :action="stage.status.action"
+            @refreshPipelineGraph="refreshPipelineGraph"
+          />
+        </ul>
+      </div>
     </div>
   </div>
 </template>
