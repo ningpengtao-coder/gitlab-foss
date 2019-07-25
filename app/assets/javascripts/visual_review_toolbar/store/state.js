3a8525545283e1cd76c1a1c9a4581c560a51bbad
sarahghp <sarah.groff.palermo@gmail.com>
2019-07-25 10:26:51 -0600

    Revert "Make the files pretty"
    This reverts commit b69302fc6a6ca98ba27e3f5f3d89b4d30800c528.
    It randolmly deleted files.

---
+++ app/assets/javascripts/visual_review_toolbar/store/state.js
@@ -0,0 +1,94 @@
+import { comment, login, mrForm,  } from '../components';
+import { localStorage, COMMENT_BOX, LOGIN, MR_ID } from '../shared';
+
+const state = {
+  browser: '',
+  href: '',
+  innerWidth: '',
+  innerHeight: '',
+  mergeRequestId: '',
+  mrUrl: '',
+  platform: '',
+  projectId: '',
+  userAgent: '',
+  token: '',
+};
+
+// adapted from https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator#Example_2_Browser_detect_and_return_an_index
+const getBrowserId = sUsrAg => {
+  /* eslint-disable-next-line @gitlab/i18n/no-non-i18n-strings */
+  const aKeys = ['MSIE', 'Edge', 'Firefox', 'Safari', 'Chrome', 'Opera'];
+  let nIdx = aKeys.length - 1;
+
+  for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx -= 1);
+  return aKeys[nIdx];
+};
+
+const nextView = (appState, form = 'none') => {
+  const formsList = {
+    [COMMENT_BOX]: currentState => (currentState.token ? mrForm : login),
+    [LOGIN]: currentState => (currentState.mergeRequestId ? comment(currentState) : mrForm),
+    [MR_ID]: currentState => (currentState.token ? comment(currentState) : login),
+    none: currentState => {
+      if (!currentState.token) {
+        return login;
+      }
+
+      if (currentState.token && !currentState.mergeRequestId) {
+        return mrForm;
+      }
+
+      return comment(currentState);
+    },
+  };
+
+  return formsList[form](appState);
+};
+
+const initializeState = (wind, doc) => {
+  const {
+    innerWidth,
+    innerHeight,
+    location: { href },
+    navigator: { platform, userAgent },
+  } = wind;
+
+  const browser = getBrowserId(userAgent);
+
+  const scriptEl = doc.getElementById('review-app-toolbar-script');
+  const { projectId, mergeRequestId, mrUrl, projectPath } = scriptEl.dataset;
+
+  // This mutates our default state object above. It's weird but it makes the linter happy.
+  Object.assign(state, {
+    browser,
+    href,
+    innerWidth,
+    innerHeight,
+    mergeRequestId,
+    mrUrl,
+    platform,
+    projectId,
+    projectPath,
+    userAgent,
+  });
+
+  return state;
+};
+
+const getInitialView = () => {
+
+  const token = localStorage.getItem('token');
+  const mrId = localStorage.getItem('mergeRequestId');
+
+  if (token) {
+    state.token = token;
+  }
+
+  if (mrId) {
+    state.mergeRequestId = mrId;
+     }
+
+  return nextView(state);
+};
+
+export { initializeState, getInitialView, nextView, state };
