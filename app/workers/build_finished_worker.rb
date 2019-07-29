60f1866af8d61283c17c59b2175161fcbd8f3b0f
Tomasz Maczukin <tomasz@maczukin.pl>
2019-07-29 13:25:42 +0200

    Add metric tracking duration of handled CI jobs

--- app/workers/build_finished_worker.rb
+++ app/workers/build_finished_worker.rb
@@ -4,6 +4,8 @@ class BuildFinishedWorker
   include ApplicationWorker
   include PipelineQueue
 
+  JOB_DURATION_SECONDS_BUCKETS = [60, 300, 600, 1800, 3600, 7200, 21600, 86400].freeze
+
   queue_namespace :pipeline_processing
 
   # rubocop: disable CodeReuse/ActiveRecord
@@ -32,5 +34,20 @@ class BuildFinishedWorker
     ArchiveTraceWorker.perform_async(build.id)
     ExpirePipelineCacheWorker.perform_async(build.pipeline_id)
     ChatNotificationWorker.perform_async(build.id) if build.pipeline.chat?
+  end
+
+  def register_duration(build)
+    runner_type = if build.runner
+               build.runner.runner_type
+             else
+               :none
+             end
+    labels = { runner_type: runner_type }
+
+    job_duration_seconds.observe(labels, build.duration)
+  end
+
+  def job_duration_seconds
+    @job_duration_seconds ||= Gitlab::Metrics.histogram(:job_duration_seconds, 'Job execution time', {}, JOB_DURATION_SECONDS_BUCKETS)
   end
 end
