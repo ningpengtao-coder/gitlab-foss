22bb961ae4fb53e43080e37c7136e821b0557149
Denys Mishunov <dmishunov@gitlab.com>
2019-08-05 02:16:27 +0200

    Select correct default branch option for a commit
    Apply suggestion to commit_sidebar/actions.vue

--- spec/javascripts/ide/components/commit_sidebar/actions_spec.js
+++ spec/javascripts/ide/components/commit_sidebar/actions_spec.js
@@ -1,30 +1,27 @@
 import Vue from 'vue';
 import store from '~/ide/stores';
-import consts from '~/ide/stores/modules/commit/constants';
 import commitActions from '~/ide/components/commit_sidebar/actions.vue';
+import consts from '~/ide/stores/modules/commit/constants';
 import { createComponentWithStore } from 'spec/helpers/vue_mount_component_helper';
 import { resetStore } from 'spec/ide/helpers';
-import { projectData } from 'spec/ide/mock_data';
+import { projectData, branches } from 'spec/ide/mock_data';
 
 describe('IDE commit sidebar actions', () => {
   let vm;
-  const createComponent = ({
-    hasMR = false,
-    commitAction = consts.COMMIT_TO_NEW_BRANCH,
-    mergeRequestsEnabled = true,
-    currentBranchId = 'master',
-    shouldCreateMR = false,
-  } = {}) => {
+  let updateCommitActionSpy;
+
+  const createComponent = ({ hasMR = false, currentBranchId = 'master' } = {}) => {
     const Component = Vue.extend(commitActions);
 
     vm = createComponentWithStore(Component, store);
 
     vm.$store.state.currentBranchId = currentBranchId;
     vm.$store.state.currentProjectId = 'abcproject';
-    vm.$store.state.commit.commitAction = commitAction;
-    Vue.set(vm.$store.state.projects, 'abcproject', { ...projectData });
-    vm.$store.state.projects.abcproject.merge_requests_enabled = mergeRequestsEnabled;
-    vm.$store.state.commit.shouldCreateMR = shouldCreateMR;
+
+    const proj = { ...projectData };
+    proj.branches[currentBranchId] = branches.find(branch => branch.name === currentBranchId);
+
+    Vue.set(vm.$store.state.projects, 'abcproject', proj);
 
     if (hasMR) {
       vm.$store.state.currentMergeRequestId = '1';
@@ -33,7 +30,9 @@ describe('IDE commit sidebar actions', () => {
       ] = { foo: 'bar' };
     }
 
-    return vm.$mount();
+    vm.$mount();
+
+    return vm;
   };
 
   afterEach(() => {
@@ -71,6 +70,136 @@ describe('IDE commit sidebar actions', () => {
       createComponent({ currentBranchId: injectedSrc });
 
       expect(vm.commitToCurrentBranchText).not.toContain(injectedSrc);
+    });
+  });
+
+  describe('updateSelectedCommitAction', () => {
+    it('does not return anything if currentBranch does not exist', () => {
+      updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+      vm.updateSelectedCommitAction();
+
+      expect(updateCommitActionSpy).not.toHaveBeenCalled();
+    });
+
+    describe('default branch', () => {
+      it('dispatches correct action for default branch', () => {
+        createComponent({
+          currentBranchId: 'master',
+        });
+        updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+        vm.updateSelectedCommitAction();
+
+        expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_NEW_BRANCH);
+      });
+    });
+
+    describe('protected branch', () => {
+      describe('with write access', () => {
+        it('dispatches correct action when MR exists', () => {
+          createComponent({
+            hasMR: true,
+            currentBranchId: 'protected/access',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_CURRENT_BRANCH);
+        });
+
+        it('dispatches correct action when MR does not exists', () => {
+          createComponent({
+            hasMR: false,
+            currentBranchId: 'protected/access',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_CURRENT_BRANCH);
+        });
+      });
+
+      describe('without write access', () => {
+        it('dispatches correct action when MR exists', () => {
+          createComponent({
+            hasMR: true,
+            currentBranchId: 'protected/no-access',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_NEW_BRANCH);
+        });
+
+        it('dispatches correct action when MR does not exists', () => {
+          createComponent({
+            hasMR: false,
+            currentBranchId: 'protected/no-access',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_NEW_BRANCH);
+        });
+      });
+    });
+
+    describe('regular branch', () => {
+      describe('with write access', () => {
+        it('dispatches correct action when MR exists', () => {
+          createComponent({
+            hasMR: true,
+            currentBranchId: 'regular',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_CURRENT_BRANCH);
+        });
+
+        it('dispatches correct action when MR does not exists', () => {
+          createComponent({
+            hasMR: false,
+            currentBranchId: 'regular',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_CURRENT_BRANCH);
+        });
+      });
+
+      describe('without write access', () => {
+        it('dispatches correct action when MR exists', () => {
+          createComponent({
+            hasMR: true,
+            currentBranchId: 'regular/no-access',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_NEW_BRANCH);
+        });
+
+        it('dispatches correct action when MR does not exists', () => {
+          createComponent({
+            hasMR: false,
+            currentBranchId: 'regular/no-access',
+          });
+          updateCommitActionSpy = spyOn(vm, 'updateCommitAction');
+
+          vm.updateSelectedCommitAction();
+
+          expect(updateCommitActionSpy).toHaveBeenCalledWith(consts.COMMIT_TO_NEW_BRANCH);
+        });
+      });
     });
   });
 });
