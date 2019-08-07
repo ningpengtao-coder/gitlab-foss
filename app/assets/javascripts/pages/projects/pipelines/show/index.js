bff42979afa5fd5396510c82eb4c7aa8cc0df9fe
mfluharty <mfluharty@gitlab.com>
2019-08-07 12:16:56 -0600

    Load merge requests into pipeline view

--- app/assets/javascripts/pages/projects/pipelines/show/index.js
+++ app/assets/javascripts/pages/projects/pipelines/show/index.js
@@ -1,7 +1,9 @@
 import initPipelineDetails from '~/pipelines/pipeline_details_bundle';
 import initPipelines from '../init_pipelines';
+import { fetchCommitMergeRequests } from '~/commit_merge_requests';
 
 document.addEventListener('DOMContentLoaded', () => {
   initPipelines();
   initPipelineDetails();
+  fetchCommitMergeRequests();
 });
