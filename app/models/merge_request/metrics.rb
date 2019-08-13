d620f62516e42c59c50eec2428f39eb5936373f9
Pavel Shutsin <pshutsin@gitlab.com>
2019-08-13 23:59:10 +0300

    Add merged_after scope for MR metrics

--- app/models/merge_request/metrics.rb
+++ app/models/merge_request/metrics.rb
@@ -5,4 +5,6 @@ class MergeRequest::Metrics < ApplicationRecord
   belongs_to :pipeline, class_name: 'Ci::Pipeline', foreign_key: :pipeline_id
   belongs_to :latest_closed_by, class_name: 'User'
   belongs_to :merged_by, class_name: 'User'
+
+  scope :merged_after, -> (date) { where('merged_at >= ?', date) }
 end
