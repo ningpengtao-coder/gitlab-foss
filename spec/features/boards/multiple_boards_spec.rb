7ade32d5365dee59b752e957fb29f6d4b3dcd1c0
Felipe Artur <felipefac@gmail.com>
2019-08-13 11:00:51 -0300

    Replace 'let' with 'set'

--- spec/features/boards/multiple_boards_spec.rb
+++ spec/features/boards/multiple_boards_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 describe 'Multiple Issue Boards', :js do
-  let(:user) { create(:user) }
-  let(:project) { create(:project, :public) }
-  let!(:planning) { create(:label, project: project, name: 'Planning') }
-  let!(:board) { create(:board, name: 'board1', project: project) }
-  let!(:board2) { create(:board, name: 'board2', project: project) }
+  set(:user) { create(:user) }
+  set(:project) { create(:project, :public) }
+  set(:planning) { create(:label, project: project, name: 'Planning') }
+  set(:board) { create(:board, name: 'board1', project: project) }
+  set(:board2) { create(:board, name: 'board2', project: project) }
   let(:parent) { project }
   let(:boards_path) { project_boards_path(project) }
 
