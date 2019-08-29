33e319c3cb3e166eefd1cc74433aeff621565f07
Sean Carroll <scarroll@gitlab.com>
2019-08-29 15:30:37 +0200

    Backfill release name with tag

---
+++ db/migrate/20190829131947_backfill_release_name_with_tag.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class BackfillReleaseNameWithTag < ActiveRecord::Migration[5.2]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  def up
+    subquery = Arel.sql("select id from projects where visibility_level = #{Gitlab::VisibilityLevel::PUBLIC}")
+
+    update_column_in_batches(:releases, :name, Release.arel_table[:tag]) do |table, query|
+      query.where(table[:name].eq(nil)).where(table[:project_id].in(subquery))
+    end
+  end
+
+  def down
+    # noop
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_083843) do
+ActiveRecord::Schema.define(version: 2019_08_29_131947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
