6dc70e9eaab9432d6ad42a5400dd926274f22aae
sstern <sstern@gitlab.com>
2019-08-30 09:47:03 -0700

    Restructure tests to clean up mocks in store_spec.js

--- spec/frontend/issue_show/store_spec.js
+++ spec/frontend/issue_show/store_spec.js
@@ -1,5 +1,7 @@
 import Store from '~/issue_show/stores';
-import * as updateDescription from '~/issue_show/utils/update_description';
+import updateDescription from '~/issue_show/utils/update_description';
+
+jest.mock('~/issue_show/utils/update_description');
 
 describe('Store', () => {
   let store;
@@ -29,38 +31,9 @@ describe('Store', () => {
     });
 
     it('calls updateDetailsState', () => {
-      /*
-       * need to mock 'default' because we need to test its functionality in the next test
-       * so we spy on its default export and then reset the module.
-       */
-      const spy = jest.spyOn(updateDescription, 'default');
-
       store.updateState({ description: '' });
 
-      expect(updateDescription.default).toHaveBeenCalledTimes(1);
-
-      spy.mockRestore();
-    });
-
-    it('returns the correct value to be set as descriptionHtml', () => {
-      store.updateState({
-        description:
-          '<details><summary>One</summary></details><details><summary>Two</summary></details>',
-      });
-
-      expect(store.state.descriptionHtml).toEqual(
-        '<details open="true"><summary>One</summary></details><details><summary>Two</summary></details>',
-      );
-    });
-
-    describe('when description details returned from api is different then whats currently on the dom', () => {
-      it('returns the description from the api', () => {
-        const dataDescription = '<details><summary>One</summary></details>';
-
-        store.updateState({ description: dataDescription });
-
-        expect(store.state.descriptionHtml).toEqual(dataDescription);
-      });
+      expect(updateDescription).toHaveBeenCalledTimes(1);
     });
   });
 });
