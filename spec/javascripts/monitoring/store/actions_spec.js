b06b951d13aaed9d7aaca2b5997b3e1afe3dacfd
Dhiraj Bodicherla <dhiraj@gitlab.com>
2019-09-05 14:34:46 -0700

    Updated spec files

--- spec/javascripts/monitoring/store/actions_spec.js
+++ spec/javascripts/monitoring/store/actions_spec.js
@@ -14,6 +14,7 @@ import {
   setEndpoints,
   setGettingStartedEmptyState,
 } from '~/monitoring/stores/actions';
+import { uniqMetricsId } from '~/monitoring/utils';
 import storeState from '~/monitoring/stores/state';
 import testAction from 'spec/helpers/vuex_action_helper';
 import { resetStore } from '../helpers';
@@ -327,7 +328,7 @@ describe('Monitoring store actions', () => {
 
       setTimeout(() => {
         expect(commit).toHaveBeenCalledWith(types.SET_QUERY_RESULT, {
-          metricId: metric.metric_id,
+          metricId: uniqMetricsId(metric),
           result: data.result,
         });
         done();
