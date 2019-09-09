2a960abb179dbff168bbc155c01d05a1bfd7ac69
Himanshu Kapoor <hkapoor@gitlab.com>
2019-09-09 22:40:23 +0530

    Reduce the amount of flicker when loading the MR page in the iframe.

--- app/assets/javascripts/ide/components/merge_requests/create.vue
+++ app/assets/javascripts/ide/components/merge_requests/create.vue
@@ -4,7 +4,7 @@ import { mapGetters, mapState } from 'vuex';
 import Icon from '../../../vue_shared/components/icon.vue';
 import TitleComponent from '../../../issue_show/components/title.vue';
 import DescriptionComponent from '../../../issue_show/components/description.vue';
-import { setTimeout } from 'timers';
+import { setTimeout, setInterval, clearInterval } from 'timers';
 
 export default {
   components: {
@@ -13,12 +13,33 @@ export default {
     DescriptionComponent,
   },
   computed: {
-    ...mapState(['currentBranchId', 'currentProjectId'])
+    ...mapState(['currentBranchId', 'currentProjectId', 'currentMergeRequestId']),
+
+    mergeRequestSrc() {
+      if (this.currentMergeRequestId) {
+        return `/${this.currentProjectId}/merge_requests/${this.currentMergeRequestId}`;
+      }
+      return `/${this.currentProjectId}/merge_requests/new?merge_request%5Bsource_branch%5D=${this.currentBranchId}&amp;merge_request%5Btarget_branch%5D=master`;
+    }
   },
   methods: {
     onload: () => {
-      $('#js-create-mr-sidebar').get(0).contentDocument.body.className += '  create-merge-request-iframe'
+
     }
+  },
+  mounted: () => {
+    let i = setInterval(() => {
+      try {
+        if ($('#js-create-mr-sidebar').get(0).contentWindow.location.href === 'about:blank') return;
+
+        $('#js-create-mr-sidebar').get(0).contentDocument.body.className += '  create-merge-request-iframe';
+        clearInterval(i);
+        console.log()
+      }
+      catch (e) {}
+    }, 1000 / 60)
+
+    setTimeout(clearInterval, 10000, i)
   }
 };
 </script>
@@ -27,7 +48,7 @@ export default {
   <div class="ide-create-merge-request d-flex flex-column h-100">
     <iframe
       id="js-create-mr-sidebar"
-      :src="`/${currentProjectId}/merge_requests/new?merge_request%5Bsource_branch%5D=${currentBranchId}&amp;merge_request%5Btarget_branch%5D=master`"
+      :src="mergeRequestSrc"
       class="h-100 border-0"
       style="margin: -8px -16px;"
       @load=onload()>
