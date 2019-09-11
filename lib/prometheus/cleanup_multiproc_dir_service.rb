21eb312530448e76467d8e36131453636a4104a1
Aleksei Lipniagov <alipniagov@gitlab.com>
2019-09-11 19:50:38 +0300

    Log metric dir cleanup, keep gauge for master
    Do not remove particular metric to investigate:
    https://gitlab.com/gitlab-org/gitlab-ce/issues/66889

--- lib/prometheus/cleanup_multiproc_dir_service.rb
+++ lib/prometheus/cleanup_multiproc_dir_service.rb
@@ -6,18 +6,29 @@ module Prometheus
 
     def execute
       FileUtils.rm_rf(old_metrics) if old_metrics
+
+      log_cleanup
     end
 
     private
 
     def old_metrics
       strong_memoize(:old_metrics) do
-        Dir[File.join(multiprocess_files_dir, '*.db')] if multiprocess_files_dir
+        if multiprocess_files_dir
+          # TODO: remove `.select {}` part after investigating
+          # https://gitlab.com/gitlab-org/gitlab-ce/issues/66889
+          Dir[File.join(multiprocess_files_dir, '*.db')].select {|f| f !~ /gauge_all_unicorn_master/}
+        end
       end
     end
 
     def multiprocess_files_dir
       ::Prometheus::Client.configuration.multiprocess_files_dir
+    end
+
+    def log_cleanup
+      message = "Cleanup Prometheus multiprocess_files_dir, pid: #{Process.pid}"
+      Gitlab::AppLogger.info(message)
     end
   end
 end
