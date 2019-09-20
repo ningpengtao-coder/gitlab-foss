b5d67c3f3512ea1572f1cebe09891d307fa9480a
Olena Horal-Koretska <ohoralkoretska@gitlab.com>
2019-09-20 16:25:28 +0100

    Data url update

--- app/assets/javascripts/AAgeneric_chart/sample_data/prometheus.js
+++ app/assets/javascripts/AAgeneric_chart/sample_data/prometheus.js
@@ -1,3 +1,5 @@
 export default {
-  url: 'http://demo.robustperception.io:9090/api/v1/query_range?query=scrape_duration_seconds&start=1567355404.093&end=1567528204.093&step=691&_=1568133003984',
+  url: 'http://demo.robustperception.io:9090/api/v1/query_range?query=scrape_duration_seconds&start=1568816468.958&end=1568820068.958&step=14&_=1568992868808',
 };
+
+//http://demo.robustperception.io:9090/graph?g0.range_input=1h&g0.end_input=2019-09-18%2015%3A21&g0.expr=scrape_duration_seconds&g0.tab=0
